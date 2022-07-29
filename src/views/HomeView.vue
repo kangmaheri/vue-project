ba44fb643cc880c2b1146b603c838432eb55e92e
kangmaheri <kangmaheri@gmail.com>
2022-07-29 08:04:16 +0700

    Absolute Basic vue 3

---
+++ src/views/HomeView.vue
@@ -0,0 +1,19 @@
+<script>
+export default {
+  data() {
+    return {
+      greeting: "Hello Big World",
+    };
+  },
+  mounted() {
+    setTimeout(() => {
+      this.greeting = "Changed";
+    }, 3000);
+  },
+};
+</script>
+
+<template>
+  <input type="text" v-model="greeting" />
+  <p>{{ greeting }} ({{ greeting.length }})</p>
+</template>
