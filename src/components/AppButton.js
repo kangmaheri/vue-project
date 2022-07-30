501b49e2374df282f84c88a44c96806697e13595
kangmaheri <kangmaheri@gmail.com>
2022-07-30 10:44:26 +0700

    component props

--- src/components/AppButton.js
+++ src/components/AppButton.js
@@ -1,15 +1,36 @@
 export default {
   data() {
-  return {
-    count: 0,
-    processing: false
-  }
+    return {
+      count: 0
+    }
   },
   /*html */
   template: `
-  <button @click="count++" :disabled="processing">
+  <button 
+    @click="count++" 
+    :disabled="processing" 
+    :class="{
+      'default' : true,
+      'blue' : type === 'primary',
+      'maroon' : type === 'secondary',  
+      'grey' : type === 'muted',
+      'is-loading': processing
+    }"
+  >
     <slot />
     You clicked me {{ count }} times. 
   </button>
-  `
+  `,
+
+  props: {
+    type: {
+      type: String,
+      default: 'primary'
+    },
+
+    processing: {
+      type: Boolean,
+      default: false,
+    }
+  },
 }
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
-    <App-button>button 1</App-button>
+    <App-button type="muted" :processing="true">button 1</App-button>
   </div>
 </template>
 
@@ -9,14 +9,48 @@
 import AppButton from '../components/AppButton.js';
 
 export default {
-  
   components: {
-    'App-button': AppButton
+   AppButton
   },
 }
 </script>
 
 <style>
+.is-loading {
+  display: inline-block;
+  color: transparent;
+}
+.is-loading:after {
+  content: " ";
+  display: block;
+  width: 24px;
+  height: 24px;
+  margin: 8px;
+  border-radius: 50%;
+  border: 6px solid #fff;
+  border-color: #fff transparent #fff transparent;
+  animation: lds-dual-ring 1.2s linear infinite;
+}
+@keyframes lds-dual-ring {
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+}
+.blue{
+  background: blue;
+}
+.default{
+  border: 1px solid black;
+}
+.maroon{
+  background: maroon;
+}
+.grey{
+  background: grey;
+}
 @media (min-width: 1024px) {
   .about {
     min-height: 100vh;
