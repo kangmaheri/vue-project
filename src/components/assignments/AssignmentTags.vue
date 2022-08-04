5d36655a78630c48b7ca6186c7f8fa4184b000d9
kangmaheri <kangmaheri@gmail.com>
2022-08-04 15:30:43 +0700

    component responsibility

---
+++ src/components/assignments/AssignmentTags.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="flex gap-2">
+      <button 
+        @click="$emit('change', tag)"
+        v-for="tag, index in tags" 
+        :key="index" 
+        class="border rounded px-1 py-px text-xs hover:bg-slate-500 hover:text-white"
+        :class="{
+          'border-blue-500 text-blue-500' : tag === currentTag
+        }"
+      >
+        {{ tag }}
+      </button>
+    </div>
+</template>
+
+<script>
+export default { 
+  props: {
+    initialTags: Array,
+    currentTag: String,
+  },
+
+  computed: {
+    tags() {
+      return ['all', ...new Set(this.initialTags)]; 
+    },
+
+  }
+}
+</script>
---
+++ src/components/assignments/assignmentCreate.vue
@@ -0,0 +1,28 @@
+<template>
+   <form @submit.prevent="add" class="border border-gray-600">
+    <input type="text" v-model="newAssignment" placeholder="New Assignment..." />
+    <button type="submit" class="border rounded">Add</button>
+  </form>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      newAssignment: '',
+    };
+  },
+
+  methods: {
+    add() {
+      this.$emit('tambah', this.newAssignment);
+      this.newAssignment = '';
+      // this.assignments.push({
+      //   name: this.newAssignment,
+      //   completed: false,
+      //   id: this.assignments.length + 1,
+      // });
+    },
+  },
+};
+</script>
